d70c6860f6c071818be42e44b61501dd0cb6f41d
Vinicius Dittgen <vinipd@gmail.com>
2023-03-21 10:40:28 -0300

    add redislite initialization code

--- web/controller.py
+++ web/controller.py
@@ -5,6 +5,9 @@ from web.views import users_blueprint
 def create_app():
     web_app = Flask(__name__)
     web_app.register_blueprint(users_blueprint)
+
+    # Initialize the RedisLite extension with the Flask app object
+    # redis_lite = RedisLite(app, data_dir='/tmp/redislite')
     return web_app
 
 
